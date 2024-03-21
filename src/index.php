e7132983edf2e41dd1267d017c5ad78168b6fbc4
jeanclaude <jeanclaudenzabaherahaza@gmail.com>
2024-03-21 11:05:36 +0200

    index page is created

---
+++ src/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DEVOPS CAT</title>
+</head>
+<body>
+    <center>
+        <h1>23rp00262 </h1>
+    <?php
+    echo"welcome ";
+    ?>
+    <br>
+    <a href="registration.php">Registration</a>
+    <a href="login.php">Login</a>
+    <center>
+</body>
+</html>
