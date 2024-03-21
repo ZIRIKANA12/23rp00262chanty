ae0052c750e234356ad0328ee2546ed3dda868d4
jeanclaude <jeanclaudenzabaherahaza@gmail.com>
2024-03-21 11:15:57 +0200

    Registration page is created

---
+++ src/registration.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    firstname:<inputtype="name" name="firstname"><br>
+    lastname:<inputtype="name" name="lastname><br>
+    gender:<inputtype="gender" name="female">female<br>
+    <inputtype="gender" name="male">male<br>
+    Email:<inputtype="email" name="email"><br>
+    password:<inputtype="password" name="password">
+</body>
+</html>
