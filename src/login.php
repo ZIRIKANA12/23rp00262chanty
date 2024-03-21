ce6c38e713fe1282132d942d5104803b2dc93d95
jeanclaude <jeanclaudenzabaherahaza@gmail.com>
2024-03-21 12:58:40 +0200

    index page is created

--- src/login.php
+++ src/login.php
@@ -1,27 +1,37 @@
-<
-<html>
+<!DOCTYPE html>
+
+<html lang="en">
+
 <head>
-<title> Registration Form</title>
+
+    <meta charset="UTF-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>REGISTRATION</title>
+
 </head>
+
 <body>
-<center>
-<h1 style=" border-radius: 15px; background:orange;height: 45px; width: 472px;padding:4px;"><b style="color:blue;">STUDENT SIMPLE FORM</b></h1>
-<fieldset style="width: 350px;background-color:skyblue">
-<legend><h4><b style="color: blue">STUDENTS REGISTRATION FORM</b></h4></legend>
-<form action="insert.php" method="POST">
-EMAIL<br>					
-<input type="text" name="email"  style="height: 25px; border-radius: 5px;">
-<br>	
-password<br>
-<input type="password" name="password"  style="height: 25px; border-radius: 5px;">			
-<form action<br><br>
-<input type="submit" name="save" value="submit" style=" border-radius: 5px; background: #17a2b8;height: 25px; width: 62px;">
-				<br>
-	
-<a href="table.php"  style="text-decoration: none;"><H3>Click Here To View Students</H3></a>
-</fieldset>
-	
+
+    <p1><i>Login Form</i></p1>
+
+    <form>
+
+   <Table>
+
+    <tr><td>Email</td><td><input type="text" name="email" ></td></tr>
+
+    <tr><td>Password</td><td><input type="password" name="pass" ></td></tr> </tr>
+
+    <tr><td><input type="submit" name="submit" Value="Login"></td></tr>
+
 </form>
-</center>
- </body> </html>
+
+</table>
+</body>
 </html>
+
+ 
+
+ 
